9efa2efcead3c8667d402f44687d594bf3483669
Dillon Fagan <dillonfagan@gmail.com>
2018-02-08 17:00:00 -0500

    Minor simplification.

--- Seven Bridges/Announcement.swift
+++ Seven Bridges/Announcement.swift
@@ -10,9 +10,9 @@ import UIKit
 class Announcement {
     
     static func new(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         
         UIApplication.shared.keyWindow?.rootViewController?.present(alert, animated: true, completion: nil)
     }
