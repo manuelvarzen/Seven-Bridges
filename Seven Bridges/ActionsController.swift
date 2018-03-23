e833983b89d28be1823b770a0aabe78b84f56925
Dillon Fagan <dillonfagan@gmail.com>
2018-03-23 18:27:51 -0400

    The new actions menu is complete and wired up

--- Seven Bridges/ActionsController.swift
+++ Seven Bridges/ActionsController.swift
@@ -16,10 +16,68 @@ class ActionsController: UITableViewController {
     weak var graph: Graph!
     
     @IBAction func close(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        dismissView()
     }
     
     @IBAction func toggleDirectedEdges(_ sender: UISwitch) {
         graph.isDirected = sender.isOn
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dismissView()
+        
+        switch indexPath.section {
+        case 1:
+            quickAction(at: indexPath.row)
+        case 2:
+            algorithm(at: indexPath.row)
+        case 3:
+            template(at: indexPath.row)
+        default:
+            print("A cell was selected in section: \(indexPath.section)")
+        }
+    }
+    
+    private func quickAction(at row: Int) {
+        switch row {
+        case 0:
+            graph.renumberNodes()
+        case 1:
+            graph.resetAllEdgeWeights()
+        case 2:
+            graph.removeAllEdges()
+        default:
+            print("A cell was selected in row: \(row)")
+        }
+    }
+    
+    private func algorithm(at row: Int) {
+        switch row {
+        case 0:
+            graph.shortestPath()
+        case 1:
+            graph.primMinimumSpanningTree()
+        case 2:
+            graph.kruskalMinimumSpanningTree()
+        case 3:
+            graph.fordFulkersonMaxFlow()
+        case 4:
+            graph.bronKerbosch()
+        default:
+            print("A cell was selected in row: \(row)")
+        }
+    }
+    
+    private func template(at row: Int) {
+        switch row {
+        case 0:
+            graph.prepareFlowNetworkExample()
+        default:
+            print("A cell was selected in row: \(row)")
+        }
+    }
+    
+    private func dismissView() {
+        dismiss(animated: true, completion: nil)
     }
 }
