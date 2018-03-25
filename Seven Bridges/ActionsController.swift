8cc7bea88ea8ae9c9b83f8af8ce337bcd9e190d4
Dillon Fagan <dillonfagan@gmail.com>
2018-03-25 11:17:32 -0400

    Added comments to ActionsController. The table view in the actions menu will now deselect cells so that the highlight doesn't persist the next time it is opened.

--- Seven Bridges/ActionsController.swift
+++ Seven Bridges/ActionsController.swift
@@ -15,15 +15,30 @@ class ActionsController: UITableViewController {
     /// The graph that the actions will be applied to.
     weak var graph: Graph!
     
+    /// Dismisses the view when the close button is tapped.
+    ///
+    /// - parameter sender: The close button.
+    ///
     @IBAction func close(_ sender: UIBarButtonItem) {
         dismissView()
     }
     
+    /// Toggles directed edges in the graph.
+    ///
+    /// - parameter sender: The toggle switch.
+    ///
     @IBAction func toggleDirectedEdges(_ sender: UISwitch) {
         graph.isDirected = sender.isOn
     }
     
+    /// Called when a cell in the table is selected.
+    /// Matches the cell to an action and executes the appropriate method.
+    ///
+    /// - parameter tableView: The table view that contains the cells.
+    /// - parameter didSelectRowAt: The section and row of the cell.
+    ///
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // skip section 0, since this only contains the directed edges toggle
         guard indexPath.section != 0 else {
             return
         }
@@ -40,8 +55,15 @@ class ActionsController: UITableViewController {
         default:
             print("A cell was selected in section: \(indexPath.section)")
         }
+        
+        // deselect the cell so that the highlight disappears
+        tableView.deselectRow(at: indexPath, animated: false)
     }
     
+    /// Executes a quick action method given a table row.
+    ///
+    /// - parameter at: The row of the cell.
+    ///
     private func quickAction(at row: Int) {
         switch row {
         case 0:
@@ -55,6 +77,10 @@ class ActionsController: UITableViewController {
         }
     }
     
+    /// Executives an algorithm method given a table row.
+    ///
+    /// - parameter at: The row of the cell.
+    ///
     private func algorithm(at row: Int) {
         switch row {
         case 0:
@@ -72,6 +98,10 @@ class ActionsController: UITableViewController {
         }
     }
     
+    /// Executes a template method given a table row.
+    ///
+    /// - parameter at: The row of the cell.
+    ///
     private func template(at row: Int) {
         switch row {
         case 0:
@@ -81,6 +111,7 @@ class ActionsController: UITableViewController {
         }
     }
     
+    /// Dismisses the view with a fade animation.
     private func dismissView() {
         dismiss(animated: true, completion: nil)
     }
